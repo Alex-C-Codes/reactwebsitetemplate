96c53ac64425dc41b68eccaeae1ed37f05e19ce5
Alex-C <alexcook1613@gmail.com>
2023-07-14 19:21:33 -0400

    used original floppy bird site as template

---
+++ floppybird/src/components/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <div>
+          <p>
+            Hello
+          </p>
+      </div>
+    </div>
+  );
+}
